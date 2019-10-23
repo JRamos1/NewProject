4ced428d9e90aa4cc4a9712e5bdc3a35fcee00ae
Justin Ramos <jmramos396@gmail.com>
2019-10-22 21:30:42 -0400

    navbar

---
+++ client/src/components/Nav/MainNav.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+import NavDropdown from "react-bootstrap/NavDropdown"
+function MainNav() {
+    return(
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Recent</Nav.Link>
+            <NavDropdown title="Relevant Topics" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Election</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Climate</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Politics</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Something else</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Sign up</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Sign in
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+}
+
+export default MainNav
